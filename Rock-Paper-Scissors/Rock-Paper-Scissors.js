67d33ee05790d4d94c9dd40d177c63fb1013635c
P.Bhargavi <pallainibhargavi@gmail.com>
2024-12-29 13:40:40 +0530

    Update Rock-Paper-Scissors.js

--- Rock-Paper-Scissors/Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors/Rock-Paper-Scissors.js
@@ -2,7 +2,10 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
-
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerscore = 0;
+let computerscore = 0;
 function playgame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     let result = "";
@@ -26,4 +29,19 @@ function playgame(playerChoice) {
     playerDisplay.textContent = `PLAYER: ${playerChoice.toUpperCase()}`;
     computerDisplay.textContent = `COMPUTER: ${computerChoice.toUpperCase()}`;
     resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greeenText","redText");
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerscore++;
+            playerScoreDisplay.textContent = playerscore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerscore++;
+            computerScoreDisplay.textContent = computerscore;
+            break;
+    }
 }
